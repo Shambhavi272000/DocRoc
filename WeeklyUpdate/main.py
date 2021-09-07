92b3b654bbe8e9e4efb156c91810a9139e4561f5
vridhiabc <vridhi.lalwani.2000@gmail.com>
2021-09-07 23:52:00 +0530

    DocRoc

---
+++ WeeklyUpdate/main.py
@@ -0,0 +1,42 @@
+import os
+from app import app
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'files[]' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	files = request.files.getlist('files[]')
+	input_nopol = request.form['text_box']
+	with open('nopol.txt', 'a') as f:
+		f.write(str(input_nopol) + '\n')
+	file_names = []
+	for file in files:
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file_names.append(filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			f = open("nopol.txt", "r")
+			text = f.read().strip()
+			print(text)
+	return render_template('upload.html', filenames=file_names,nopol=input_nopol,text=text)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=9999)
--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+from subprocess import Popen, PIPE
 import sys
 
 
 def main():
+    imageUpload()
+    os.chdir(r"C:\Users\Lenovo\Desktop\Documents\GitHub\DocRoc")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospitalmanagement.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,5 +20,11 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+def imageUpload():
+    os.chdir(r"C:\Users\Lenovo\Desktop\Documents\GitHub\DocRoc\WeeklyUpdate")
+    p = Popen(['python', 'main.py'], stdin=PIPE, bufsize=0)
+    return ('')
+
 if __name__ == '__main__':
     main()
+  
